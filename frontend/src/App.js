34f0c681f8336f9785ae3dc8c28dc454ae8be63e
amareLD <80774058+amareLD@users.noreply.github.com>
2023-06-12 15:06:09 +0530

    create e-commerce react app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Attire Avenue</a>
+        
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
