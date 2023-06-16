2ff5ab04b04fd2d7a5b57334041509e3693725a7
amareLD <80774058+amareLD@users.noreply.github.com>
2023-06-16 09:44:13 +0530

    add productscreen & homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Attire Avenue</a>
-        
-      </header>
-      <main>List Products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Attire Avenue</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
